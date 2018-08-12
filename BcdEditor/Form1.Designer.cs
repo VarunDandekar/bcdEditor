83e81f9ff4e74a54e93ae138ff671ba8abfdc265
VarunDandekar <varundandekar95@gmail.com>
2018-08-12 22:40:18 +0530

    bcdeditor

---
+++ BcdEditor/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace BcdEditor
+{
+    partial class BcdEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel2);
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel1);
+
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(485, 236);
+            this.splitContainer1.SplitterDistance = 135;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Controls.Add(this.button3);
+            this.flowLayoutPanel2.Controls.Add(this.checkedListBox1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(101, 79);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(282, 51);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 49);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 79);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(482, 71);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(27, 10, 10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter WinPE Path:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(239, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(27, 4, 10, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(461, 81);
+            this.label2.TabIndex = 0;
+            // 
+            // BcdEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(485, 236);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.MaximizeBox = false;
+            this.Name = "BcdEditor";
+            this.Text = "BcdEditor";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ BcdEditor/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BcdEditor
+{
+    public partial class BcdEditor : Form
+    {
+        string path = "";
+        string guid = "3ddd0b4b-54c6-4088-805b-3bb618e2597d";
+        public BcdEditor()
+        {
+            InitializeComponent();
+        }
+        delegate void SetTextCallback(string text);
+
+
+        private void SetText(string text)
+        {
+
+            using (StreamWriter w = File.AppendText("myFile.txt"))
+            {
+                w.WriteLine(text);
+            }
+            // invokerequired required compares the thread id of the
+            // calling thread to the thread id of the creating thread.
+            // if these threads are different, it returns true.
+            if (this.label2.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label2.Text = text;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            path = textBox1.Text;
+            if (!File.Exists(path))
+            {
+                label2.Text = "File not found";
+                return;
+            }
+            if (Path.GetExtension(path) != ".WIM")
+            {
+                label2.Text = "Invalid File Format(.WIM expected)";
+                return;
+            }
+            AddEntry(path,guid);
+            label2.Text = "Entry Added";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+
+            openFileDialog1.Title = "Browse WinPe File";
+
+            openFileDialog1.CheckFileExists = true;
+
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "wim";
+
+            openFileDialog1.Filter = "windows image files (*.wim)|*.wim|All files (*.*)|*.*";
+
+            openFileDialog1.FilterIndex = 2;
+
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        public void AddEntry(string path, string guid)
+        {
+            int colon = path.IndexOf(":");
+            path = path.Substring(0, colon - 1) + "[" + path.Substring(colon - 1, colon+1) + "]" + path.Substring(colon+1);
+            SetText(path);
+            try
+            {
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /delete {"+guid+"}");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /create {" + guid + "} /d 'Mayfair' /application osloader");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /displayorder {" + guid + "} /addlast");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} device ramdisk=" + path + ",{ramdiskoptions}");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} osdevice ramdisk=" + path + ",{ramdiskoptions}");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} path \\Windows\\sysnative\\Boot\\winload.exe");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} locale en-Us");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} systemroot \\Windows");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} detecthal Yes");
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /set {" + guid + "} winpe Yes");
+            }
+            catch (Exception e)
+            {
+                SetText(e.Message);
+            }
+        }
+
+        public void DeleteEntry(string guid)
+        {
+            try
+            {
+                ExecuteCommand("%systemroot%\\sysnative\\bcdedit /delete {" + guid + "}");
+            }
+            catch (Exception e)
+            {
+                SetText(e.Message);
+            }
+        }
+
+        public void ExecuteCommand(string Command)
+        {
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("cmd", "/C "+ Command)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                //Verb = "runas",
+                CreateNoWindow = true
+            };
+
+            Process process = Process.Start(startInfo);
+            if ( process != null)
+            {
+                process.Start();
+                SetText("output: " + process.StandardOutput.ReadToEnd());
+                SetText("error: " + process.StandardError.ReadToEnd());
+                process.WaitForExit();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteEntry(guid);
+            label2.Text = "Entry Deleted";
+        }
+
+    }
+}
